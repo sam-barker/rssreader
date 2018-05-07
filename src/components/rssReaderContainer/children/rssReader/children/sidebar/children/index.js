6b4c954df116fdffe7ee670afd762af242a38453
Samuel Barker <spbarker1994@gmail.com>
2018-05-07 12:05:29 +0100

    refactored styles

--- src/components/rssReaderContainer/children/rssReader/children/sidebar/children/index.js
+++ src/components/rssReaderContainer/children/rssReader/children/sidebar/children/index.js
@@ -1,2 +1,3 @@
 export {default as Search} from './search'
 export {default as FeedList} from './feedList'
+export {default as AddFeed} from './addFeed'
--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,1 +1,3 @@
 export {default as Toggle} from './toggle'
+export {default as InputField} from './inputField'
+export {default as SubmitButton} from './submitButton'
