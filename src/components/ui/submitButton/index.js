eba596b3e51ea6afd18123ad3572ebf645cc54cc
Samuel Barker <spbarker1994@gmail.com>
2018-05-13 20:41:08 +0100

    refactored submit button

--- src/components/ui/submitButton/index.js
+++ src/components/ui/submitButton/index.js
@@ -1,24 +1,17 @@
-import React, {Component} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Styles from './styles.scss'
 
-class SubmitButton extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {}
-  }
-
-  render () {
-    return (
-      <div
-        className={Styles[`button${this.props.size}`]}
-        onClick={this.props.onClick}>
-        <button className={Styles.button} disabled={this.props.disabled}>
-          {this.props.text}
-        </button>
-      </div>
-    )
-  }
+function SubmitButton (props) {
+  return (
+    <div
+      className={Styles[`button${props.size}`]}
+      onClick={props.onClick}>
+      <button className={Styles.button} disabled={props.disabled}>
+        {props.text}
+      </button>
+    </div>
+  )
 }
 
 SubmitButton.defaultProps = {
